dca647383d86c6da1d0c2c0fc03d98363924bdcf
Singularity Labs <ishandutta2007@gmail.com>
2026-07-05 07:08:37 +0530

    fixed star plot

---
+++ fix_chartrepos.py
@@ -0,0 +1,11 @@
+import os
+
+readme_path = r'C:\Users\ishan\Documents\Projects\Awesome-Artificial-General-Intelligence\README.md'
+
+with open(readme_path, 'r', encoding='utf-8') as f:
+    content = f.read()
+
+content = content.replace("chartrepos", "chart?repos")
+
+with open(readme_path, 'w', encoding='utf-8') as f:
+    f.write(content)
